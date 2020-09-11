907fd4e6b2924506e5d61daeadcfa1a652ba5bab
Chris Bitsakis <c.bitsakis@gmail.com>
2020-09-10 22:57:42 -0400

    v1.0.0-beta

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,9 @@
+import * as clipperLib from 'js-angusj-clipper'
 import clipperOffset from './offset'
 import clipperUnite from './unite'
 
 export {
+  clipperLib,
   clipperOffset,
   clipperUnite
 }
