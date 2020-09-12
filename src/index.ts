0adc44a9b5136ebc61b8f364f71a02a5d76de6a6
Chris Bitsakis <c.bitsakis@gmail.com>
2020-09-12 16:33:28 -0400

    clipperLib and clipperLibWeb

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,11 @@
 import * as clipperLib from 'js-angusj-clipper'
+import * as clipperLibWeb from 'js-angusj-clipper/web'
 import clipperOffset from './offset'
 import clipperUnite from './unite'
 
 export {
   clipperLib,
+  clipperLibWeb,
   clipperOffset,
   clipperUnite
 }
